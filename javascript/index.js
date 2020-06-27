9d8b0dfc58c31c82a08bf80d2e2fcdedd5678ed0
Kimz <57965210+Kimz2@users.noreply.github.com>
2020-06-27 10:31:43 -0700

    java fixed

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-window.onload = ()=> {
+// window.onload = ()=> {
 let myMenu = document.getElementById("js-myMenu");
 let hamburger = document.getElementById("js-hamburger");
 
@@ -6,4 +6,4 @@ hamburger.addEventListener("click", function() {
   myMenu.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
-};
+// };
